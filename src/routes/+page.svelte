11cb2681670b084d09edabe57e6c44408022dc80
hunter <104146303+solhosty@users.noreply.github.com>
2023-08-26 12:17:41 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="justify-center m-auto flex mt-4">
+	<div class="card w-96 bg-base-100 shadow-xl image-full">
+		<figure><img src="favicon.png" alt="Shoes" /></figure>
+		<div class="card-body">
+			<h2 class="card-title">Template</h2>
+			<p>This is a base template with Sveltekit, Tailwind, and DaisyUI</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary">Buy Now</button>
+			</div>
+		</div>
+	</div>
+</div>
