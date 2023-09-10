e1f32275b9a3cc48e76967ecb0968ba537a30798
hunter <104146303+solhosty@users.noreply.github.com>
2023-09-10 12:39:57 -0400

    supabase connection

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,18 @@
-// @ts-nocheck
+// src/hooks.server.ts
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
+import type { Handle } from '@sveltejs/kit'
 
-export const handle = async ({ event, resolve }) => {
+export const handle: Handle = async ({ event, resolve }) => {
   event.locals.supabase = createSupabaseServerClient({
     supabaseUrl: PUBLIC_SUPABASE_URL,
     supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
     event,
   })
+
+  /**
+   * A convenience helper so we can just call await getSession() instead const { data: { session } } = await supabase.auth.getSession()
+   */
   event.locals.getSession = async () => {
     const {
       data: { session },
--- src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import '../app.postcss';
-</script>
-
-<div> 
-	<slot />
-
-</div>
